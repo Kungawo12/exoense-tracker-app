10e693c6fd66ab00e0c209a3cee0cec88202145d
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2024-07-29 22:44:24 -0500

    added crud function to the project

--- app/page.js
+++ app/page.js
@@ -1,37 +1,93 @@
 'use client'
 import React, {useState,useEffect} from 'react';
-import { collection, addDoc } from "firebase/firestore"; 
+import { 
+  collection, 
+  addDoc, 
+  getDoc, 
+  querySnapshot,
+  query, 
+  onSnapshot,
+  deleteDoc,
+  doc, 
+} from "firebase/firestore"; 
+import { db } from './firebase';
 
 export default function Home() {
   const [items, setitems] = useState([
-    {name:'Coffee', price: 4.99},
-    {name:'Movie', price: 12.99},
-    {name:'dinner', price: 29.99},
+    // {name:'Coffee', price: 4.99},
+    // {name:'Movie', price: 12.99},
+    // {name:'dinner', price: 29.99},
   ]);
-  const [newItem, setNewItem] = useState({name: '', price: })
+  const [newItem, setNewItem] = useState({name: '', price:'' })
   const [total, setTotal] = useState(0);
-  return (
+  
 
     // Add item to database 
-    const addItem= async(e)=>{
-      e.preventDefault()
+  const addItem= async (e) => {
+    e.preventDefault();
+    if (newItem.name != '' && newItem.price != ''){
+      // setitems([...items,newItem]);
+      await addDoc(collection(db, 'items'),{
+      name: newItem.name.trim(),
+      price: newItem.price,
     }
+      )};
+      setNewItem({name: '', price:''})
+    };
+
 
     // Read items from database
+    useEffect(() => {
+      const q= query(collection(db,'items'))
+      const unsubscribe =onSnapshot(q,(querySnapshot) => {
+        let itemsArr = []
+
+        querySnapshot.forEach((doc) => {
+          itemsArr.push({...doc.data(),id: doc.id})
+        });
+        setitems(itemsArr)
+
+        //Read total from itemsArr
+        const calculateTotal = () => {
+          const totalPrice = itemsArr.reduce((sum, item) => sum + parseFloat(item.price), 0)
+          setTotal(totalPrice)
+        }
+        calculateTotal()
+        return () => unsubscribe()
+      })
+    },[])
 
     // Delete item from database
-
-
+    const deleteItem = async (id) => {
+      await deleteDoc(doc(db, 'items', id));
+    }
+    return (
+      
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className='text-4xl p-4 text-center'>Expense Tracker</h1>
         <div className="bg-slate-800 p-4 rounded-lg">
           <form className="grid grid-cols-6 items-center text-black">
             <input 
-            value={}
-            className="col-span-3 p-3 border" type="text" placeholder="Enter Item"/>
-            <input className="col-span-2 p-3 border mx-3" type="number" placeholder="Enter $" />
-            <button className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl"type="submit">+</button>
+            value={newItem.name}
+            onChange={(e) => setNewItem({...newItem, name: e.target.value})}
+            className="col-span-3 p-3 border" 
+            type="text" 
+            placeholder="Enter Item"
+            />
+            <input 
+            value={newItem.price}
+            onChange={(e) => setNewItem({...newItem, price: e.target.value})}
+            className="col-span-2 p-3 border mx-3" 
+            type="number" 
+            placeholder="Enter $" />
+            <button 
+            onClick={addItem}
+            className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl"
+            type="submit"
+            >
+              +
+              </button>
           </form>
         <ul>
           {items.map((item,id)=>(
@@ -40,14 +96,14 @@ export default function Home() {
                 <span className='capitalize'>{item.name}</span>
                 <span>${item.price}</span>
               </div>
-              <button className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'>X</button>
+              <button onClick={() => deleteItem(item.id)} className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'>X</button>
             </li>
           ))}
         </ul>
         {items.length < 1 ?(''):(
           <div className='flex justify-between p-3'>
             <span>Total</span>
-            <span>$Total</span>
+            <span>${total}</span>
             </div>
         )}
         </div>
